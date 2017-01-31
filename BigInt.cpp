ddd6ef365899ef56e5e7d91a733b932537bd38e7
Tommy Zng <1319524930@qq.com>
2017-01-31 20:30:49 +0800

    Update BigInt.cpp

--- BigInt.cpp
+++ BigInt.cpp
@@ -31,7 +31,7 @@ namespace __impl{
 			if(!siz)return 0;
 			ui len=siz,ret=0;
 			for(ui i=1;i<len;++i)
-				coe[i]+=coe[i-1]>>32;
+				coe[i]+=long(coe[i-1])>>32;
 			ret=coe[len-1]>>32;
 			for(ui i=0;i<len;++i)
 				coe[i]&=mask;
@@ -61,13 +61,13 @@ namespace __impl{
 			siz=max(a.siz,b.siz);
 			return *this;
 		}
-		inline bool smaller(self a,bool automatic_normalization=1){
+		inline bool smaller(self&a,bool automatic_normalization=1){
 			if(automatic_normalization)Normalize(),a.Normalize();
 			if(siz<a.siz)return 1;if(siz>a.siz)return 0;
 			for(ui t=siz-1;~t;--t)if(coe[t]<a.coe[t])return 1;else if(coe[t]>a.coe[t])return 0;
 			return 0;
 		}
-		bool greater(self a,bool automatic_normalization=1){
+		bool greater(self&a,bool automatic_normalization=1){
 			return a.smaller(*this,automatic_normalization);
 		}
 		self&Diff(self b,bool size_check=1){//make this the difference
@@ -77,12 +77,14 @@ namespace __impl{
 		}
 		self&To_diff(self&b,bool size_check=1){//make b the difference
 			if(size_check&&smaller(b))return b.Diff(*this,0);
+//			b.Print();puts("To diff called: b");
+//			Print();puts("To diff called: this");
 			b.FitTo(alc),b.siz=siz;
 			for(ui i=siz-1;~i;--i)b.coe[i]=coe[i]-b.coe[i];
 			return b.Normalize();
 		}
 		self Cop(){
-			self g(all);g.Resize(alc);
+			self g(all);g.Resize(alc);g.siz=siz;
 			memcpy(g.coe,coe,siz*sizeof(uf));
 			return g;
 		}
@@ -101,11 +103,54 @@ namespace __impl{
 			memset(coe,0,sizeof(uf)*siz);
 			siz=0;return *this;
 		}
+		inline self&divexact_3(){
+			uf c=0,l,q;
+			for(ui i=0;i<siz;++i){
+				c=(l=coe[i]-c)>>63;
+				q=l*0xAAAAAAABu&mask;
+				coe[i]=q;
+				c+=q*3>>32;
+			}return *this;
+		}
+		inline self&divexact_3_pip(){
+			uf l=0,c=0,q,s,t;
+			for(ui i=0;i<siz;++i){
+				t=(s=coe[i])*0xAAAAAAAB&mask;
+				coe[i]=q=(t-l)&mask;
+				c=(q+(s<c))*3>>32;
+				l=(long(0-c)>>63)&(0xAAAAAAAB<<c-1);
+			}return *this;
+		}
+		inline self&rshb(ui z){
+			if(!z)return *this;
+			if(z>siz)return memset(coe,0,sizeof(uf)*siz),siz=0,*this;
+			for(ui t=0,_=siz-z;t<_;++t)coe[t]=coe[t+z];
+			return memset(coe+(siz-z),0,sizeof(uf)*z),siz=siz-z,*this;
+		}
+		inline self&rshs(ui z){
+			if(!z||!siz)return *this;
+			ui p=(1ul<<z)-1;
+			for(ui t=0,_=siz-1;t<_;++t)coe[t]=(coe[t]>>z)+((coe[t+1]&p)<<32-z);
+			coe[siz-1]>>=z;
+			return *this;
+		}
+		inline self&rsh(ui t){return this->rshb(t>>5)->rshs(t&31);}
+		inline self&div2(){
+			if(!siz)return *this;
+			for(ui t=0,_=siz-1;t<_;++t)coe[t]=(coe[t]>>1)+((coe[t+1]&1)<<31);
+			return coe[siz-1]>>=1,*this;
+		}
+		inline self&mul2(){
+			for(ui t=0;t<siz;++t)coe[t]<<=1;
+			return *this;
+		}
 	};
 
 	#define tbal template<class Allocator>
 	#define bal bui<Allocator>
-	
+
+	#include "SignedOperation.cpp"
+
 	tbal bal Mult_All(bal&a,bal&b,Allocator all);
 	tbal inline bal Mult_school(bal&a,bal&b,Allocator all){
 		bal c(all);a.Normalize(),b.Normalize();c.Resize(a.siz+b.siz);
