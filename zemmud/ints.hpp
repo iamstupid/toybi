1fa1511730cac50f1cab7ba9483f6e24e29395a8
Tommy Zng <1319524930@qq.com>
2017-05-08 19:45:08 +0800

    Create ints.hpp

---
+++ zemmud/ints.hpp
@@ -0,0 +1,13 @@
+#ifndef ints_h
+#define ints_h
+#include "stdint.h"
+typedef uint32_t u32;
+typedef uint64_t u64;
+#ifdef __uint128_t
+typedef __uint128_t u128;
+#endif
+typedef float f32;
+typedef double f64;
+typedef long double fee;
+
+#endif
