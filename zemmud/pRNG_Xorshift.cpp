86c8c8eda7cefdda54805a78b0f4156bac8415aa
Tommy Zng <1319524930@qq.com>
2017-05-09 01:59:40 +0800

    Update pRNG_Xorshift.cpp
    Completed all suggested xorshift rngs on wiki page..

--- zemmud/pRNG_Xorshift.cpp
+++ zemmud/pRNG_Xorshift.cpp
@@ -22,6 +22,60 @@ namespace pRNG{
 		template<typename T>
 		inline void seed(const T*buffer_type){memset(s,buffer_type,sizeof s);}
 		inline u32 operator()(){
+			u32 g=s[3];
+			g^=g<<11;
+			g^=g>>8;
+			s[3]=s[2];s[2]=s[1];s[1]=s[0];
+			return s[0]=((g^=s[0])^=s[0]>>19);
+		}
+	};
+	struct xss64{
+		typedef u64 result_type;
+		u64 s;
+		xss64(u64 s=0x9bf4bd1d835a38e6ull):s(s){}
+		inline void seed(u64 st){s=st;}
+		inline u64 operator()(){
+			s^=s>>12;
+			s^=s<<25;
+			return (s^=s>>27)*0x2545f4914f6cdd1dull;
+		}
+	};
+	struct xss1k{
+		typedef u64 result_type;
+		u64 s[16],p;
+		xss1k():
+		s{	0x79a3d197a3eda564ull,0xea0832f5f13eca6eull,
+			0x9b25d666c98ad37eull,0xc47e8b052d0159dfull,
+			0xc3d03d041c267d8dull,0xfea949190d0bf9f8ull,
+			0x07f1e5bc39068da5ull,0x44679e2a74432b86ull,
+			0x205ca0fd8db63493ull,0x3c086aaa6dd55a74ull,
+			0xc4e064b3e570282bull,0x4c7b4621df4da54dull,
+			0x7b14f5648cbe10cdull,0x848656ce83c494a5ull,
+			0xb02648187107686dull,0x964d7d7714d55e34ull},
+		p(0){
+			}// a really long initializer list; in fact generated with /dev/random
+		template<typename T>
+		xss1k(const T*buffer_type){memset(s,buffer_type,sizeof s);}
+		template<typename T>
+		inline void seed(const T*buffer_type){memset(s,buffer_type,sizeof s);}
+		inline u64 operator()(){
+			register u64 s0=s[p],s1=s[++p&=15];
+			s1^=s1<<31;
+			return (s[p]=s1^s0^(s1>>11)^(s0>>30))*0x106689d45497fdb5ull;
+		}
+	};
+	struct xsadw{
+		typedef u64 result_type;
+		u64 s[2];
+		xsadw():s{0xc4f7ba40435d8ee7ull,0x54d21665a4290c80ull}{}
+		template<typename T>
+		xsadw(const T*buffer_type){memset(s,buffer_type,sizeof s);}
+		template<typename T>
+		inline void seed(const T*buffer_type){memset(s,buffer_type,sizeof s);}
+		inline u64 operator()(){
+			u64 x=*s,y=s[1];
+			x^=x<<23;
+			return (s[1]=x^(*s=y)^(x>>17)^(y>>26))+y;
 		}
 	};
 }
